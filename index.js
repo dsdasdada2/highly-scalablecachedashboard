ec3169549490e8fb85bc701dc27744b95c59715b
dsdasdada2 <gs1chato762@hotmail.com>
2024-05-10 23:53:12 +0700

    add more features

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const intersection = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => s.has(x));
-};
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) left = mid + 1;
+    else if (nums[mid] < nums[right]) right = mid;
+    else right--;
+  }
+  return nums[left];
+}
