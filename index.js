47c556b8b3f906a0b2d009bc163a36cba679e4cf
dsdasdada2 <gs1chato762@hotmail.com>
2024-05-13 20:53:28 +0700

    commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-function sortedArrayToBST(nums) {
-  if (nums.length === 0) return null;
-  const mid = Math.floor(nums.length / 2);
-  const root = new TreeNode(nums[mid]);
-  root.left = sortedArrayToBST(nums.slice(0, mid));
-  root.right = sortedArrayToBST(nums.slice(mid + 1));
-  return root;
+function groupAnagrams(strs) {
+  const map = new Map();
+  for (const str of strs) {
+    const sortedStr = str.split("").sort().join("");
+    if (!map.has(sortedStr)) map.set(sortedStr, []);
+    map.get(sortedStr).push(str);
+  }
+  return [...map.values()];
 }
