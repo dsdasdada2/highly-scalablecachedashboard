46e04f3175c3a8bda1dad930346ef3e3c36a0f7c
dsdasdada2 <gs1chato762@hotmail.com>
2024-05-14 19:13:16 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function groupAnagrams(strs) {
-  const map = new Map();
-  for (const str of strs) {
-    const sortedStr = str.split("").sort().join("");
-    if (!map.has(sortedStr)) map.set(sortedStr, []);
-    map.get(sortedStr).push(str);
+function findOrder(numCourses, prerequisites) {
+  const graph = new Array(numCourses).fill(0).map(() => []);
+  const inDegree = new Array(numCourses).fill(0);
+  for (const [course, pre] of prerequisites) {
+    graph[pre].push(course);
+    inDegree[course]++;
   }
-  return [...map.values()];
+  const queue = [];
+  for (let i = 0; i < numCourses; i++) {
+    if (inDegree[i] === 0) queue.push(i);
+  }
+  const result = [];
+  while (queue.length) {
+    const course = queue.shift();
+    result.push(course);
+    for (const nextCourse of graph[course]) {
+      inDegree[nextCourse]--;
+      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
+    }
+  }
+  return result.length === numCourses ? result : [];
 }
