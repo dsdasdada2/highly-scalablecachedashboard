9ab1988baab89166cfb33b2d186759506b640e57
dsdasdada2 <gs1chato762@hotmail.com>
2024-05-15 12:48:06 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-function findOrder(numCourses, prerequisites) {
-  const graph = new Array(numCourses).fill(0).map(() => []);
-  const inDegree = new Array(numCourses).fill(0);
-  for (const [course, pre] of prerequisites) {
-    graph[pre].push(course);
-    inDegree[course]++;
-  }
-  const queue = [];
-  for (let i = 0; i < numCourses; i++) {
-    if (inDegree[i] === 0) queue.push(i);
-  }
-  const result = [];
-  while (queue.length) {
-    const course = queue.shift();
-    result.push(course);
-    for (const nextCourse of graph[course]) {
-      inDegree[nextCourse]--;
-      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
+const gnomeSort = (arr) => {
+  let index = 0;
+  while (index < arr.length) {
+    if (index === 0 || arr[index] >= arr[index - 1]) {
+      index++;
+    } else {
+      [arr[index], arr[index - 1]] = [arr[index - 1], arr[index]];
+      index--;
     }
   }
-  return result.length === numCourses ? result : [];
-}
+  return arr;
+};
